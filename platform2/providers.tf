7a5201f4bc83b51857768f5ea0e0ce49884e5173
Rayane <rayane.zekik@epita.fr>
2024-10-12 11:51:55 +0200

    fix: removed state backend

--- platform2/providers.tf
+++ platform2/providers.tf
@@ -9,14 +9,14 @@ terraform {
       version = "~>2.0"
     }*/
   }
-  backend "s3" {
+ /* backend "s3" {
     encrypt        = true
     bucket         = "ga-bucket"
     dynamodb_table = "terraform-state-lock-dynamo"
     key            = "state/terraform.tfstate"
     region         = "eu-west-3"
-  }
-  required_version = ">= 1.2.0"
+  } */
+  required_version = ">= 1.2.0" 
 }
 
 // Specifies provider parameters
