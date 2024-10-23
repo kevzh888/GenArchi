c6496768c22ff62142d724e90c3f829b379a8fbc
Vincent NGUYEN <vincent1.nguyen@epita.fr>
2024-10-23 12:21:18 +0200

    feat(output): ip db

--- platform2/terraform/modules/database_ec2/output.tf
+++ platform2/terraform/modules/database_ec2/output.tf
@@ -2,3 +2,11 @@
 #    description = "IP addresses of database shard servers"
 #    value       = aws_instance.db_shard.*.public_ip
 #}
+
+output "db_instance_private_ip" {
+  value = aws_instance.db_instance.private_ip
+}
+
+output "db_instance_public_ip" {
+  value = aws_instance.db_instance.public_ip
+}
