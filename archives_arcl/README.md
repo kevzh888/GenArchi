b918f734deaa847fac9c55ed92ecc861ddefde74
Vincent NGUYEN <vincent1.nguyen@epita.fr>
2024-10-28 20:36:04 +0100

    refactor(archi): arcl deleted

--- archives_arcl/README.md
+++
@@ -1,6 +0,0 @@
-# Gen Archi
-
-## Platform 2
-
-Requirement:
-- Faut créer un bucket S3
--- archives_arcl/modules/vpc/output.tf
+++
@@ -1,9 +0,0 @@
-output "vpc-id" {
-  description = "Default VPC id."
-  value       = aws_vpc.vpc.id
-}
-
-output "route-table-id" {
-  description = "Route table id"
-  value       = aws_route_table.default_route_table.id
-}
